221281c72ae3208eb03020b9e9da47381714bd35
Shalini Kumari <shalinik@iitbhilai.ac.in>
2024-04-06 17:21:43 +0530

    added api endpoint

--- src/main/java/Controller/StockController.java
+++ src/main/java/Controller/StockController.java
@@ -5,22 +5,22 @@ import StockServices.StockService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/stock")
+@RequestMapping("/api/stocks")
 @AllArgsConstructor
 public class StockController {
     @Autowired
     private StockService stockService;
 
-    @GetMapping("/popularStocks")
-    public List<Stock> getPopularStocks(){
-        return (List<Stock>) stockService.getStockDetail("abc");
+    @GetMapping("/{id}")
+    public Stock getStockById(@PathVariable String id){
+        return (Stock) stockService.getStockDetail(id);
     }
-
 
 }
--- src/main/java/Model/Stock.java
+++ src/main/java/Model/Stock.java
@@ -5,17 +5,32 @@ import org.springframework.data.annotation.Id;
 public class Stock{
 
     @Id
+    private String isin;
+    private String growwContractId;
     private String companyName;
-
-    private String companyShortName ;
-    private int industryCode;
+    private String companyShortName;
     private String searchId;
+    private int industryCode;
+    private int bseScriptCode;
+    private String nseScriptCode;
+    private double yearlyHighPrice;
+    private double yearlyLowPrice;
+    private double closePrice;
+    private double marketCap;
 
-    public Stock(String companyName, String companyShortName, int industryCode, String searchId) {
+    public Stock(String isin, String growwContractId, String companyName, String companyShortName, int industryCode, String searchId, int bseScriptCode, String nseScriptCode, double yearlyHighPrice, double yearlyLowPrice, double closePrice, double marketCap) {
         super();
+        this.isin = isin;
+        this.growwContractId = growwContractId;
         this.companyName = companyName;
         this.companyShortName = companyShortName;
         this.industryCode = industryCode;
         this.searchId = searchId;
+        this.bseScriptCode = bseScriptCode;
+        this.nseScriptCode = nseScriptCode;
+        this.yearlyHighPrice = yearlyHighPrice;
+        this.yearlyLowPrice = yearlyLowPrice;
+        this.closePrice = closePrice;
+        this.marketCap = marketCap;
     }
 }
